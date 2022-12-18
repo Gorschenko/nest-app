ffa5999f3d78b64b8c19526935afca0e69914ae2
Alexey <aa.gorschenko@yandex.ru>
2022-12-18 17:49:56 +0300

    DB has been added

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,30 @@
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoConfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => {
+  return {
+    uri: getMongoString(configService),
+    ...getMongoOptions()
+  }
+}
+
+const getMongoString = (configService: ConfigService) => {
+  return 'mongodb://' +
+    configService.get('MONGO_LOGIN') +
+    ':' +
+    configService.get('MONGO_PASSWORD') +
+    '@' +
+    configService.get('MONGO_HOST') +
+    ':' +
+    configService.get('MONGO_PORT') +
+    '/' +
+    configService.get('MONGO_AUTHDATABASE')
+}
+
+const getMongoOptions = () => {
+  return {
+    useNewUrlParses: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+}
