17632e309f9b667eba88c18e1e78fb46d896393d
Alexey <aa.gorschenko@yandex.ru>
2022-12-20 23:00:18 +0300

    DB base models have been connected

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { TopPageController } from './top-page.controller';
 
 @Module({
-  controllers: [TopPageController]
+  controllers: [TopPageController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: TopPageModule,
+        schemaOptions: {
+          collection: 'TopPage',
+        }
+      }
+    ])
+  ],
 })
 export class TopPageModule {}
