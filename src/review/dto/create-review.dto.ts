7cf7f39d8303d57a689539bf85c414284614daf2
Alexey <aa.gorschenko@yandex.ru>
2022-12-22 22:12:09 +0300

    Validation has been initialized

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, Min, Max } from 'class-validator/types/decorator/decorators'
+
 export class CreateReviewDto {
+    @IsString()
     name: string
+
+    @IsString()
     title: string
+
+    @IsString()
     description: string
+
+    @Max(5)
+    @Min(1)
+    @IsNumber()
     rating: number
+
+    @IsString()
     productId: string
 }
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.conctants';
 import { ReviewService } from './review.service';
+
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto)
